6103a1b2f80d250dcd8558f49e75d4c0a3871e24
duansiman <epdcsiman@163.com>
2017-07-05 10:45:40 +0800

    mysql

--- blog_python/mysqldb.py
+++ blog_python/mysqldb.py
@@ -6,47 +6,48 @@ from mysql.connector import errorcode
 
 class DBManager:
 
-    username = 'root'
-    password = '123456'
-    database = 'blog'
-    host = 'localhost'
+	username = 'root'
+	password = '123456'
+	database = 'blog'
+	host = 'localhost'
 
-    def connect(self):
-    	config = {
-    		'user':DBManager.username,
-    		'password':DBManager.password,
-    		'host':DBManager.host,
-            'database':DBManager.database
-    	}
-	
-    	try:
-    		cnx = mysql.connector.connect(**config)
-            return cnx
-    	except mysql.connector.Error as err:
-    		if (err.errno == errorcode.ER_ACCESS_DENIED_ERROR):
-    			print("The credentials you provided are not correct.")
-    		elif (err.errno == errorcode.ER_BAD_DB_ERROR):
-    			print("The database you provided does not exist.")
-    		else:
-    			print("Something went wrong: " , err)
-            return None
+	def connect(self):
+		config = {
+			'user':DBManager.username,
+			'password':DBManager.password,
+			'host':DBManager.host,
+			'database':DBManager.database
+		}
 
-    def execute(self, sql, data):
-        connection = self.connect()
-        if not connection:
-            return False
+		try:
+			cnx = mysql.connector.connect(**config)
+			return cnx
 
-        cur = connection.cursor()
-        try:
-            cur.execute(sql, data)
-            connection.commit()
-            return True
-        except mysql.connector.Error as err:
-            print("An error occured: {}".format(err))
-            return False
-        finally:
-            cur.close()
-            connection.close()
+		except mysql.connector.Error as err:
+			if (err.errno == errorcode.ER_ACCESS_DENIED_ERROR):
+				print("The credentials you provided are not correct.")
+			elif (err.errno == errorcode.ER_BAD_DB_ERROR):
+				print("The database you provided does not exist.")
+			else:
+				print("Something went wrong: " , err)
+			return None
+
+	def execute(self, sql, data):
+		connection = self.connect()
+		if not connection:
+			return False
+
+		cur = connection.cursor()
+		try:
+			cur.execute(sql, data)
+			connection.commit()
+			return True
+		except mysql.connector.Error as err:
+			print("An error occured: {}".format(err))
+			return False
+		finally:
+			cur.close()
+			connection.close()
 
 				
 		
